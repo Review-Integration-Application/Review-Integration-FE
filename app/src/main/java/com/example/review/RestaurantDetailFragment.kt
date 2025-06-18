6de6d767fc77730e59e0c6416117749e1608bd83
202110894 <ghdekqls821@naver.com>
2025-06-18 18:15:34 +0900

    feat: 메인에서 전달 받은 데이터 적용, 각 상세 프래그먼트 이동, 백버튼 리스터 추가 #1

--- app/src/main/java/com/example/review/RestaurantDetailFragment.kt
+++ app/src/main/java/com/example/review/RestaurantDetailFragment.kt
@@ -9,31 +9,67 @@ import com.example.review.databinding.FragmentRestaurantDetailBinding
 
 class RestaurantDetailFragment: Fragment() {
 
-    lateinit var binding: FragmentRestaurantDetailBinding
+    companion object {
+        private const val ARG_NAME = "name"
+        private const val ARG_DESC = "desc"
+
+        fun newInstance(name: String, desc: String): RestaurantDetailFragment {
+            val fragment = RestaurantDetailFragment()
+            val args = Bundle()
+            args.putString(ARG_NAME, name)
+            args.putString(ARG_DESC, desc)
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    private var _binding: FragmentRestaurantDetailBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentRestaurantDetailBinding.inflate(inflater, container, false)
+        _binding = FragmentRestaurantDetailBinding.inflate(inflater, container, false)
 
         // 네이버 리뷰 버튼 클릭 시
-//        binding.naverReviewBt.setOnClickListener {
-//            parentFragmentManager.beginTransaction()
-//                .replace(R.id.fragmentContainer, RestaurantDetailNaverFragment())
-//                .addToBackStack(null)
-//                .commitAllowingStateLoss()
-//        }
-//
-//        // 카카오 리뷰 버튼 클릭 시
-//        binding.kakaoReviewBt.setOnClickListener {
-//            parentFragmentManager.beginTransaction()
-//                .replace(R.id.fragmentContainer, RestaurantDetailKakaoFragment())
-//                .addToBackStack(null)
-//                .commitAllowingStateLoss()
-//        }
+        binding.naverReviewBt.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.main, RestaurantDetailNaverFragment())
+                .addToBackStack(null)
+                .commitAllowingStateLoss()
+        }
+
+        // 카카오 리뷰 버튼 클릭 시
+        binding.kakaoReviewBt.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.main, RestaurantDetailKakaoFragment())
+                .addToBackStack(null)
+                .commitAllowingStateLoss()
+        }
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val name = arguments?.getString(ARG_NAME)
+        val desc = arguments?.getString(ARG_DESC)
+
+        // 바인딩해서 각 뷰에 세팅
+        binding.restaurantDetailTitleTv.text = name ?: "이름없음"
+        binding.reviewSummationTv.text = desc ?: ""
+
+        // 백버튼 클릭 시
+        binding.backButtonIv.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
