7fa41082754db6aaba47bcdaa8f0aa00d77f171f
202110894 <ghdekqls821@naver.com>
2025-06-21 15:38:57 +0900

    build: 카카오 sdk 설정 #3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    //implementation(libs.identity.android.legacy)
+    //implementation(libs.identity.credential.android)
     testImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
@@ -58,4 +60,13 @@ dependencies {
     //glide
     implementation ("com.github.bumptech.glide:glide:4.13.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.13.0")
+
+    // 카카오톡 로그인 모듈 설정
+    implementation("com.kakao.sdk:v2-all:2.20.3") // 전체 모듈 설치, 2.11.0 버전부터 지원
+    implementation("com.kakao.sdk:v2-user:2.20.3") // 카카오 로그인 API 모듈
+    implementation ("com.kakao.sdk:v2-share:2.20.3") // 카카오톡 공유 API 모듈
+    implementation ("com.kakao.sdk:v2-talk:2.20.3") // 카카오톡 채널, 카카오톡 소셜, 카카오톡 메시지 API 모듈
+    implementation ("com.kakao.sdk:v2-friend:2.20.3") // 피커 API 모듈
+    implementation ("com.kakao.sdk:v2-navi:2.20.3") // 카카오내비 API 모듈
+    implementation ("com.kakao.sdk:v2-cert:2.20.3") // 카카오톡 인증 서비스 API 모듈
 }
