78e21a7d40bf7b8e563d8b9697815ff9855f4c81
202110894 <ghdekqls821@naver.com>
2025-07-21 20:18:07 +0900

    feat: 식당 조회 응답 클래스 생성 #5

---
+++ app/src/main/java/com/example/review/api/Response/RestaurantResponse.kt
@@ -0,0 +1,13 @@
+package com.example.review.api.Response
+
+data class RestaurantResponse(
+    var restore_ID: Int,
+    var restore_name: String,
+    var restore_score: Double,
+    var category: String,
+    var review_short: String?,
+    var image: String?, // 이미지는 여러 개면 리스트로!
+    var total_reviews_num: Int,
+    var lat: Double,
+    var lng: Double
+)
