30e190f9d2821c3a13f799b6a5e2ce9d6e505cb2
202110894 <ghdekqls821@naver.com>
2025-07-21 21:01:37 +0900

    feat: 식당 상세 조회 인터페이스 생성 #5

--- app/src/main/java/com/example/review/api/RestoreItf.kt
+++ app/src/main/java/com/example/review/api/RestoreItf.kt
@@ -1,11 +1,18 @@
 package com.example.review.api
 
+import com.example.review.api.Response.RestaurantDetailResponse
 import retrofit2.Call
 import com.example.review.api.Response.RestaurantResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface RestoreItf {
 
+    // 식당 전체 조회
     @GET("/restaurants")
     fun getRestaurants(): Call<List<RestaurantResponse>>
+
+    // 식당 상세 조회
+    @GET("/restaurant/{restore_id}")
+    fun getRestaurant(@Path("restore_id") restoreId: Int): Call<RestaurantDetailResponse>
 }
