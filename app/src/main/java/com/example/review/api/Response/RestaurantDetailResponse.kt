4d597cf503f19bae3ac6404236ebe556f996bc61
202110894 <ghdekqls821@naver.com>
2025-08-26 14:00:02 +0900

    feat: add 이미지 변수 #5

--- app/src/main/java/com/example/review/api/Response/RestaurantDetailResponse.kt
+++ app/src/main/java/com/example/review/api/Response/RestaurantDetailResponse.kt
@@ -1,8 +1,10 @@
 package com.example.review.api.Response
 
+// 메인 액티비티의 바텀 시트와 상세 프래그먼트
 data class RestaurantDetailResponse(
     var restore_name: String,
     var category: String,
     var total_reviews_num: Int,
-    var review_short: String?
+    var review_short: String?,
+    var image: String
 )
