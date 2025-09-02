494d146a8255e04b7b0892443dcdf19b22fb4966
202110894 <ghdekqls821@naver.com>
2025-09-02 21:18:12 +0900

    feat: 응답 클래스 변수 이름 변경 #5

--- app/src/main/java/com/example/review/api/Response/RestaurantResponse.kt
+++ app/src/main/java/com/example/review/api/Response/RestaurantResponse.kt
@@ -1,14 +1,17 @@
 package com.example.review.api.Response
 
+// 식당 DB 응답 클래스
 data class RestaurantResponse(
-    var restore_ID: Int,
-    var restore_name: String,
-    var restore_score: Double,
+    var store_id: Int,
+    var store_name: String,
+    //var restore_score: Double,
     var category: String,
-    var review_short: String?,
-    var image: String?, // 이미지는 여러 개면 리스트로!
-    var total_reviews_num: Int,
-    var lat: Double,
-    var lng: Double,
-    var location: String
+    var address: String,
+    var img_urls: String?, // 이미지는 여러 개면 리스트로!
+    var naver_review_num: Int,
+    var kakao_review_num: Int,
+    var total_review_num: Int,
+    var naver_review_summary: String?,
+    var kakao_review_summary: String?,
+    var review_summary: String?
 )
