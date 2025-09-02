375a3fc89253c16903bc88931cddeb248cb89f19
202110894 <ghdekqls821@naver.com>
2025-09-02 21:36:23 +0900

    build: TabLayoutMediator #5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,8 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
+
+    // TabLayoutMediator
+    implementation("com.google.android.material:material:1.12.0")
+
 }
