0e465ad326b497fe26a829a32e9124471eb5f50c
202110894 <ghdekqls821@naver.com>
2025-09-02 21:18:19 +0900

    feat: 응답 클래스 변수 이름 변경 #5

--- app/src/main/java/com/example/review/api/Response/RestaurantDetailResponse.kt
+++ app/src/main/java/com/example/review/api/Response/RestaurantDetailResponse.kt
@@ -2,9 +2,9 @@ package com.example.review.api.Response
 
 // 메인 액티비티의 바텀 시트와 상세 프래그먼트
 data class RestaurantDetailResponse(
-    var restore_name: String,
-    var category: String,
-    var total_reviews_num: Int,
-    var review_short: String?,
-    var image: String
+    var store_name: String?,
+    var category: String?,
+    var total_review_num: Int?,
+    var review_summary: String?,
+    var img_urls: String?
 )
