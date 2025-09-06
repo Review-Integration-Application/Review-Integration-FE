ffcad203452e547fbda97204e3066e71d3e03e17
202110894 <ghdekqls821@naver.com>
2025-09-06 14:49:57 +0900

    feat: 네이버 리뷰 API 인터페이스 추가 #5

--- app/src/main/java/com/example/review/api/RestoreItf.kt
+++ app/src/main/java/com/example/review/api/RestoreItf.kt
@@ -1,5 +1,6 @@
 package com.example.review.api
 
+import com.example.review.api.Response.NaverReviewResponse
 import com.example.review.api.Response.RestaurantDetailResponse
 import retrofit2.Call
 import com.example.review.api.Response.RestaurantResponse
@@ -15,4 +16,9 @@ interface RestoreItf {
     // 식당 상세 조회
     @GET("restaurant/{store_id}")
     fun getRestaurant(@Path("store_id") storeId: Int): Call<RestaurantDetailResponse>
+
+    // 네이버 리뷰 조회
+    @GET("naver_review/{store_id}")
+    fun getNaverReview(@Path("store_id") storeId: Int): Call<List<NaverReviewResponse>>
+
 }
