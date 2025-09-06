228a44358dd1f9a9321fb29f3438c904f9c68164
202110894 <ghdekqls821@naver.com>
2025-09-06 14:50:13 +0900

    feat: 서버 연동 로그 추가 #5

--- app/src/main/java/com/example/review/api/RetrofitBaseObj.kt
+++ app/src/main/java/com/example/review/api/RetrofitBaseObj.kt
@@ -1,5 +1,7 @@
 package com.example.review.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -11,9 +13,14 @@ object RetrofitBaseObj { // Retrofit 객체 생성
     private const val BASE_URL = "http://192.168.200.195:8000/"
 
     fun getRetrofit(): Retrofit {
+
+        val logging = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+        val client = OkHttpClient.Builder().addInterceptor(logging).build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
 
         return retrofit
