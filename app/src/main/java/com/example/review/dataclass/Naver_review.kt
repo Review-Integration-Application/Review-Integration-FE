bc1a6a0ff57ccaa211ceb339b85fffa6f1f07336
202110894 <ghdekqls821@naver.com>
2025-09-06 14:48:25 +0900

    feat: 네이버 리뷰 DB 이미지 타입 수정 #5

--- app/src/main/java/com/example/review/dataclass/Naver_review.kt
+++ app/src/main/java/com/example/review/dataclass/Naver_review.kt
@@ -1,9 +1,7 @@
 package com.example.review.dataclass
 
 data class Naver_review(
-    var rating: Float,
     var date: String,
     var content: String,
-    var image1: Int? = null,
-    var image2: Int? = null
+    val imageUrls: List<String> = emptyList()
 )
